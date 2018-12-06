8d694fa1a2240e9bd42a5ba05a0df45303229338
Shivank Rao <shivankrao@Shivanks-MacBook-Pro.local>
2018-12-06 10:59:16 -0500

    set up local db/django heroku

--- tunr_react/settings.py
+++ tunr_react/settings.py
@@ -11,8 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import django_heroku
-django_heroku.settings(locals())
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -40,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'django_extensions'
+    'django_extensions',
+    'django_heroku'
 ]
 
 MIDDLEWARE = [
@@ -80,9 +80,9 @@ WSGI_APPLICATION = 'tunr_react.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'sample_database',
-        'USER': 'sample',
-        'PASSWORD': 'sample',
+        'NAME': 'tunr_react',
+        'USER': 'tunr_admin',
+        'PASSWORD': 'tunr_admin',
         'HOST': 'localhost'
     }
 }
@@ -125,4 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+#configure django app for heroku
+import django_heroku
+django_heroku.settings(locals())
 
